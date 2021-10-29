7465a182b4692dd9be5b621666d4b4eb2b3934a6
Mat <yangju90@163.com>
2021-10-29 16:38:02 +0800

    Create CrashHandler.java

---
+++ app/src/main/java/indi/mat/work/android/util/CrashHandler.java
@@ -0,0 +1,108 @@
+public class CrashHandler implements Thread.UncaughtExceptionHandler {
+
+    public static final String TAG = "CrashHandler";
+
+    private Thread.UncaughtExceptionHandler mDefaultHandler;
+
+    private static CrashHandler INSTANCE = new CrashHandler();
+
+    private Context mContext;
+
+    private Map<String, String> infos = new HashMap<String, String>();
+
+    private DateFormat formatter = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+
+    private CrashHandler() {
+    }
+
+    public static CrashHandler getInstance() {
+        return INSTANCE;
+    }
+
+    public void init(Context context) {
+        mContext = context;
+        mDefaultHandler = Thread.getDefaultUncaughtExceptionHandler();
+        Thread.setDefaultUncaughtExceptionHandler(this);
+    }
+
+    @Override
+    public void uncaughtException(Thread thread, Throwable ex) {
+        handleException(ex);
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            LogUtil.e(TAG, "error interrupter !");
+        }
+        ActivityCollector.finishAll();
+        mDefaultHandler.uncaughtException(thread, ex);
+    }
+
+    private boolean handleException(Throwable ex) {
+        if (ex == null) {
+            return false;
+        }
+
+        new Thread() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                Toast.makeText(mContext, "App Crashed!", Toast.LENGTH_LONG).show();
+                Looper.loop();
+            }
+        }.start();
+        collectDeviceInfo(mContext);
+        String errorInfo = crashInfoToString(ex);
+        LogUtil.e(TAG, errorInfo);
+        return true;
+    }
+
+
+    public void collectDeviceInfo(Context ctx) {
+        try {
+            PackageManager pm = ctx.getPackageManager();
+            PackageInfo pi = pm.getPackageInfo(ctx.getPackageName(), PackageManager.GET_ACTIVITIES);
+            if (pi != null) {
+                String versionName = pi.versionName == null ? "null" : pi.versionName;
+                String versionCode = pi.versionCode + "";
+                infos.put("versionName", versionName);
+                infos.put("versionCode", versionCode);
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            LogUtil.e(TAG, "an error occured when collect package info" + crashInfoToString(e));
+        }
+        Field[] fields = Build.class.getDeclaredFields();
+        for (Field field : fields) {
+            try {
+                field.setAccessible(true);
+                infos.put(field.getName(), field.get(null).toString());
+                LogUtil.e(TAG, field.getName() + " : " + field.get(null));
+            } catch (Exception e) {
+                LogUtil.e(TAG, "an error occured when collect crash info" + crashInfoToString(e));
+            }
+        }
+    }
+
+    private String crashInfoToString(Throwable ex) {
+
+        StringBuffer sb = new StringBuffer();
+        for (Map.Entry<String, String> entry : infos.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            sb.append(key + "=" + value + "\n");
+        }
+
+        Writer writer = new StringWriter();
+        PrintWriter printWriter = new PrintWriter(writer);
+        ex.printStackTrace(printWriter);
+        Throwable cause = ex.getCause();
+        while (cause != null) {
+            cause.printStackTrace(printWriter);
+            cause = cause.getCause();
+        }
+        printWriter.close();
+        String result = writer.toString();
+        sb.append(result);
+
+        return sb.toString();
+    }
+}
