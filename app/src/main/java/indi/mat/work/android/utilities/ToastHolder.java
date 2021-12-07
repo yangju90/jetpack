8eafbc7d1ef4e61b67a75f4fecc7e9d67e8a8622
Mat <yangju90@163.com>
2021-12-07 09:42:43 +0800

    Update ToastHolder.java

--- app/src/main/java/indi/mat/work/android/utilities/ToastHolder.java
+++ app/src/main/java/indi/mat/work/android/utilities/ToastHolder.java
@@ -1,19 +1,28 @@
 package indi.mat.work.android.utilities;
 
+
 import android.content.Context;
+import android.widget.Toast;
+
 
 public class ToastHolder {
 
+    private static Toast toast;
 
     public static void showToast(Context context, String msg) {
+        if(toast == null){
+            toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
+//            toast.setGravity(Gravity.CENTER, 0, 0);
+        }else{
+            toast.cancel();
+            toast = Toast.makeText(context, msg, Toast.LENGTH_SHORT);
 
+            toast.setDuration(Toast.LENGTH_SHORT);
+        }
+        toast.show();
     }
 
     public static void showToast(String msg) {
         showToast(MainApplication.getAppContext(), msg);
-    }
-
-    public static void showErrorToast(String msg) {
-
     }
 }
