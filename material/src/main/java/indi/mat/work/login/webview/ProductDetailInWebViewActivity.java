f2d1efdb5d46ed247d769354edfa99d52ed55c6e
yangju90 <yangju90@163.com>
2022-01-14 23:27:58 +0800

    Web View Activity

---
+++ material/src/main/java/indi/mat/work/login/webview/ProductDetailInWebViewActivity.java
@@ -0,0 +1,16 @@
+package indi.mat.work.login.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import indi.mat.work.login.R;
+
+public class ProductDetailInWebViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_product_detail_in_web_view);
+    }
+}
